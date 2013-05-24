3688c5c2b2a5b8a5aed68bc1148026a9ebc5eca2
Antti Sykäri <antti.sykari@gmail.com>
2013-05-24 12:36:48 +0300

    Deploy: also deploy node_modules.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -18,6 +18,9 @@ ssh $TARGETHOST sudo rm -rf /opt/apps/previous/$NAME
 ssh $TARGETHOST sudo mv /opt/apps/$NAME /opt/apps/previous/$NAME
 ssh $TARGETHOST mkdir -p /opt/apps/$NAME
 rsync -r --exclude .git --exclude-from=.gitignore . $TARGETHOST:/opt/apps/$NAME
+# TODO find out the best method to deploy modules.
+# Fresh npm install would do it, but it's a bit slow.
+rsync -r -l --safe-links node_modules $TARGETHOST:/opt/apps/$NAME/
 ssh $TARGETHOST sudo chown blog.blog -R /opt/apps/$NAME
 ssh $TARGETHOST sudo cp /opt/apps/$NAME/deploy/$NAME.conf /etc/init/
 ssh $TARGETHOST "sudo stop $NAME; sudo start $NAME"
