f76f595406d075b51d6b3281f862c19114823747
Antti Sykäri <antti.sykari@gmail.com>
2013-05-30 09:54:20 +0300

    Support non-numeric ids.

--- server/run.js
+++ server/run.js
@@ -15,12 +15,11 @@ log("Server starting, env: " + env);
 
 var filenames = fs.readdirSync(__dirname + "/../posts");
 
-var validFile = /^(\d)+(\.pre)?$/;
+var validFile = /^(.+?)(\.pre)?$/;
 var postFiles = filenames.filter(function(filename) {
 	return filename.match(validFile);
 }).map(function(filename) {
 	var match = filename.match(validFile);
-	log("Match is " + match[2]);
 	return {
 		id: match[1],
 		filename: filename,
@@ -28,11 +27,25 @@ var postFiles = filenames.filter(function(filename) {
 	};
 });
 
+// Posts will contain posts ordered by date.
+var posts = [];
+
+// This is used to look up posts by id.
+var postsById = {};
+
 postFiles.forEach(function(it, n) {
-	log("postFile " + n + " has id " + it.id);
+	var post = readPost(it.filename, it.type);
+	post.id = it.id;
+	posts.push(post);
 });
 
-postFiles.sort(function(a, b) { return Number(a.id) - Number(b.id); });
+posts.sort(function(a, b) { return a.date.getTime() - b.date.getTime(); });
+posts.forEach(function(post, idx) {
+	post.idx = idx;
+	postsById[post.id] = post;
+});
+
+/*
 var latestPostId = postFiles[postFiles.length - 1].id;
 
 var posts = Object.create(null);
@@ -40,6 +53,8 @@ var posts = Object.create(null);
 postFiles.forEach(function(postFile) {
 	posts[postFile.id] = readPost(postFile.filename, postFile.type);
 });
+
+*/
 
 function formatDate(date) {
 	var months = [
@@ -89,10 +104,13 @@ app.use(listener);
 
 function listener(req, res) {
 	console.log(req.url);
-	var match = req.url.match(/^\/(\d*)$/);
-	if (match) {
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		serve(match[1] || latestPostId);
+	var match = req.url.match(/^\/(.*)$/);
+//	console.log("Match! " + JSON.stringify(match));
+	var id = match[1];
+	if (id.length == 0) {
+		serve(posts[posts.length - 1].id);
+	} else if (postsById[id]) {
+		serve(id);
 	} else if (req.url == '/posts') {
 		servePosts();
 	} else {
@@ -119,10 +137,13 @@ function listener(req, res) {
 	}
 
 	function serve(id) {
-		if (posts[id]) {
-			res.write(preamble.replace('$title', 'Blog - ' + posts[id].title));
+		if (postsById[id]) {
+			var post = postsById[id];
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write(preamble.replace('$title', 'Blog - ' + post.title));
 			res.write(links(id));
-			res.write(posts[id].content || 'Post not found');
+			res.write(post.content || 'Post not found');
+			res.end();
 			res.end(links(id));
 		} else {
 			res.writeHead(404);
@@ -134,9 +155,9 @@ function listener(req, res) {
 		res.write(preamble.replace('$title', 'Blog - all posts'));
 		res.write("<h1>All posts</h1>");
 		res.write("<ul>");
-		for (id in posts) {
-			var post = posts[id];
-			res.write("<li><a href='/" + id + "'>" + post.title + "</a>");
+		for (var i = 0; i < posts.length; i++) {
+			var post = posts[i];
+			res.write("<li><a href='/" + post.id + "'>" + post.title + "</a>");
 			if (post.date) {
 				res.write(" - <time datetime='" + post.date.toISOString()
 					+ "'>" + formatDate(post.date) + "</time>");
@@ -154,14 +175,19 @@ try {
 }
 
 function links(id) {
-	id = Number(id);
+	post = postsById[id];
 	var result = "<div class=links>";
-	if (id > 1)
+	idx = post.idx
+	if (idx > 0) {
+		var prevPost = posts[idx - 1];
 		result += "<a class='prev' "
-			+ "href='/" + (id - 1) + "'>" + posts[id - 1].title + "</a>";
-	if (id < Number(latestPostId))
+			+ "href='/" + prevPost.id + "'>" + prevPost.title + "</a>";
+	}
+	if (idx < posts.length - 1) {
+		var nextPost = posts[idx + 1];
 		result += "<a class='next' "
-			+ "href='/" + (id + 1) + "'>" + posts[id + 1].title + "</a>";
+			+ "href='/" + nextPost.id + "'>" + nextPost.title + "</a>";
+	}
 	result += "</div>";
 	return result;
 }
